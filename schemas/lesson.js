63cb02fe1f5b1fc1eac6ee3f9361bc14283fbee9
Kevin Sparling <kevinsparling@gmail.com>
2020-09-22 23:48:42 +0800

    updated, added lesson

---
+++ schemas/lesson.js
@@ -0,0 +1,43 @@
+export default {
+  name: 'lesson',
+  title: 'Lesson',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string'
+    },
+    {
+      name: 'level',
+      title: 'Student Level',
+      type: 'string'
+    },
+    {
+      name: 'image',
+      title: 'Banner Image',
+      type: 'image',
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'array',
+      of: [
+        {
+          title: 'Block',
+          type: 'block',
+          styles: [{title: 'Normal', value: 'normal'}],
+          lists: []
+        }
+      ]
+    }
+  ],
+  preview: {
+    select: {
+      title: 'title'
+    }
+  }
+}
